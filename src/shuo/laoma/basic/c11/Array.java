20e25464cdba88f7772309caf5c3b5a9939de14f
seachal <seachalzhang@gmail.com>
2019-04-08 21:00:48 +0800

    array

--- src/shuo/laoma/basic/c11/Array.java
+++ src/shuo/laoma/basic/c11/Array.java
@@ -10,10 +10,17 @@ public class Array {
 
 	public static void main(String[] args) {
 	    int[] arr = {10,20,30,40};
+//		 这种写法是错误的int[] arr2 = new int[2]{2,3};
+	    int[] arr2 = new int[]{2,3};
 	    reset(arr);
 	    for(int i=0;i<arr.length;i++){
 	        System.out.println(arr[i]);
 	    }
+
+		int[] arrA = {1,2,3};
+	    int[] arrB = {4,5,6,7};
+	    arrA = arrB;
+
 	}
 
 }
