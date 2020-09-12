7d57dcd3d9c00c80881cd9b536d7c97924c53517
Seachal <seachalzhang@gmail.com>
2020-09-12 11:03:32 +0800

    第六部分 动态与函数式编程
    第21章 反射

--- src/shuo/laoma/dynamic/c84/SimpleMapperDemo.java
+++ src/shuo/laoma/dynamic/c84/SimpleMapperDemo.java
@@ -25,6 +25,8 @@ public class SimpleMapperDemo {
 	public static void main(String[] args) {
 		Student zhangsan = new Student("张三", 18, 89d);
 		String str = SimpleMapper.toString(zhangsan);
+
+		System.out.println("sca:序列化前:"+str);
 		Student zhangsan2 = (Student) SimpleMapper.fromString(str);
 		System.out.println(zhangsan2);
 	}
